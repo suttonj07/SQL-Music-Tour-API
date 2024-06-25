0fa6e8c5263aa58fcdbe389b3cef11b0e83c0aaf
suttonj07 <suttonj07@gmail.com>
2024-06-25 10:56:52 -0400

    crud updates

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// // SEQUELIZE CONNECTION
+// const sequelize = new Sequelize(process.env.PG_URI)
+
+// try {
+//     sequelize.authenticate() 
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+// } catch(err) {
+//     console.log(`Unable to connect to PG: ${err}`) 
+// }
+
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,6 +26,11 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
